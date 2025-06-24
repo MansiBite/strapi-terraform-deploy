32eab4fd8ae044b0efe2eed7dddb872135e1a785
Mansi Bite <mansibite123@gmail.com>
2025-06-24 23:29:32 +0530

    Task Completed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "strapi_url" {
-  description = "Access your Strapi app using this IP"
-  value       = "http://${aws_ecs_service.strapi_service1.network_configuration[0].assign_public_ip ? "Check ECS console for Public IP" : "Only internal access"}"
+  description = "Access your Strapi app using the ECS public IP"
+  value       = "Deployed! Check ECS Task Public IP from AWS Console → ECS → Tasks"
 }
