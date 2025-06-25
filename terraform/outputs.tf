db102499c76414ea78420b283dbf3d99c059a61e
Mansi Bite <mansibite123@gmail.com>
2025-06-25 18:58:19 +0530

    Task 9

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "strapi_url" {
+  description = "Access your Strapi app using the ECS public IP"
+  value       = "Deployed! Check ECS Task Public IP from AWS Console → ECS → Tasks"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "image_url" {
+  default = "057569470997.dkr.ecr.ap-south-1.amazonaws.com/mansibite:latest"
+}
+
+variable "app_port" {
+  default = 1337
+}
+
+# Strapi secrets from GitHub Actions
+variable "app_keys" {}
+variable "admin_jwt_secret" {}
+variable "api_token_salt" {}
+variable "transfer_token_salt" {}
+variable "encryption_key" {} 
