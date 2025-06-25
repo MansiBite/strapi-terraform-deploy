94e1ad428182c722b18d8d3b7b713b8b6949b76b
Mansi Bite <mansibite123@gmail.com>
2025-06-25 16:40:23 +0530

    Task 8 Completed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,4 +2,3 @@ output "strapi_url" {
   description = "Access your Strapi app using the ECS public IP"
   value       = "Deployed! Check ECS Task Public IP from AWS Console → ECS → Tasks"
 }
-#Updated
