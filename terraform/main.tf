8f3ad7aa93a2f729abc44611003e5da59974c2f0
Mansi Bite <mansibite123@gmail.com>
2025-06-27 00:37:25 +0530

    Task 11

--- terraform/main.tf
+++ terraform/main.tf
@@ -311,11 +311,3 @@ resource "aws_codedeploy_deployment_group" "strapi_dg" {
   }
 }
 
-# --------------------------------------------
-# ✅ CloudWatch Dashboard Output
-# --------------------------------------------
-
-output "strapi_url" {
-  description = "Access your Strapi app using the ALB"
-  value       = aws_lb.strapi_alb.dns_name
-}
